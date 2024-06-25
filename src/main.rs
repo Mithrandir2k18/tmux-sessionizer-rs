0cc6f5106afdd8e80341d69f8d076dd13e9032ac
Mithrandir2k18 <themithrandir09@gmail.com>
2024-06-25 18:46:29 +0200

    fix: refactor and prevent some duplicate searching

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use path_clean::PathClean;
 use rayon::prelude::*;
 use serde::Deserialize;
 use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
-use std::sync::{Arc, Mutex};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -16,53 +16,34 @@ struct Cli {
         help = "Path to YAML configuration file"
     )]
     config: Option<PathBuf>,
-    #[structopt(short, long, help = "Search for and include nested git repos")]
-    nested: bool,
 }
 
 #[derive(Debug, Deserialize)]
 struct Config {
     search_paths: Vec<Option<String>>,
+    nested: Option<bool>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::from_args();
-    let nested: bool = args.nested;
-    let config: Config = if let Some(config_path) = args.config {
-        let config_content = fs::read_to_string(config_path)?;
-        match serde_yaml::from_str(&config_content) {
-            Ok(config) => config,
-            Err(e) => {
-                eprintln!("Failed to parse configuration file: {}", e);
-                std::process::exit(1);
-            }
-        }
-    } else {
-        eprintln!("Configuration file is required.");
-        std::process::exit(1);
-    };
+    let config = load_config(args.config);
+    let nested = config.nested.unwrap_or(false);
 
-    let search_paths: Vec<String> = config
-        .search_paths
-        .into_iter()
-        .filter_map(|item| item)
+    let search_paths = filter_contained_paths(config.search_paths);
+
+    let repos: Vec<PathBuf> = search_paths
+        .par_iter()
+        .filter_map(|root| {
+            if root.exists() {
+                Some(find_git_repos(root, nested))
+            } else {
+                eprintln!("Path does not exist: {}", root.display());
+                None
+            }
+        })
+        .flatten()
         .collect();
 
-    let repos: Arc<Mutex<Vec<PathBuf>>> = Arc::new(Mutex::new(Vec::new()));
-    search_paths.par_iter().for_each(|root| {
-        let root_path = shellexpand::tilde(root).to_string();
-        let root_path = Path::new(&root_path);
-
-        if root_path.exists() {
-            let found_repos = find_git_repos(root_path, nested);
-            let mut repos_lock = repos.lock().unwrap();
-            repos_lock.extend(found_repos);
-        } else {
-            eprintln!("Path does not exist: {}", root_path.display());
-        }
-    });
-
-    let repos = Arc::try_unwrap(repos).unwrap().into_inner().unwrap();
     let choices = repos
         .iter()
         .map(|p| p.display().to_string())
@@ -81,44 +62,49 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap()
         .replace('.', "_");
 
-    let tmux_running = Command::new("pgrep").arg("tmux").output()?.status.success();
-
-    if env::var("TMUX").is_err() && !tmux_running {
-        Command::new("tmux")
-            .arg("new-session")
-            .arg("-s")
-            .arg(&selected_name)
-            .arg("-c")
-            .arg(selected_path)
-            .status()?;
-        return Ok(());
+    if !is_tmux_running() {
+        start_tmux_session(&selected_name, &selected_path)?;
     }
 
-    let has_session = Command::new("tmux")
-        .arg("has-session")
-        .arg("-t")
-        .arg(&selected_name)
-        .output()?
-        .status
-        .success();
-
-    if !has_session {
-        Command::new("tmux")
-            .arg("new-session")
-            .arg("-ds")
-            .arg(&selected_name)
-            .arg("-c")
-            .arg(selected_path)
-            .status()?;
-    }
-
-    Command::new("tmux")
-        .arg("switch-client")
-        .arg("-t")
-        .arg(&selected_name)
-        .status()?;
+    switch_tmux_client(&selected_name, &selected_path)?;
 
     Ok(())
+}
+
+fn load_config(config_path: Option<PathBuf>) -> Config {
+    if let Some(path) = config_path {
+        let config_content = fs::read_to_string(path).expect("Failed to read configuration file");
+        serde_yaml::from_str(&config_content).expect("Failed to parse configuration file")
+    } else {
+        eprintln!("Configuration file is required.");
+        std::process::exit(1);
+    }
+}
+
+fn filter_contained_paths(paths: Vec<Option<String>>) -> Vec<PathBuf> {
+    let mut expanded_cleaned_paths: Vec<PathBuf> = paths
+        .into_iter()
+        .filter_map(|item| item.map(PathBuf::from))
+        .map(|p| shellexpand::tilde(p.to_str().unwrap()).to_string())
+        .map(|p| PathBuf::from(p).clean())
+        .collect();
+
+
+    expanded_cleaned_paths.sort();
+    expanded_cleaned_paths.dedup();
+
+    let mut result = Vec::new();
+
+    for path in &expanded_cleaned_paths {
+        if !expanded_cleaned_paths
+            .iter()
+            .any(|other| other != path && path.starts_with(other))
+        {
+            result.push(path.clone());
+        }
+    }
+
+    result
 }
 
 fn find_git_repos(root: &Path, nested: bool) -> Vec<PathBuf> {
@@ -126,29 +112,23 @@ fn find_git_repos(root: &Path, nested: bool) -> Vec<PathBuf> {
         return Vec::new();
     }
 
+    let mut git_repos = Vec::new();
+
     let entries: Vec<_> = fs::read_dir(root).unwrap().filter_map(Result::ok).collect();
+    for entry in entries {
+        let path = entry.path();
+        if !path.is_dir() {
+            continue;
+        }
+        if path.join(".git").exists() {
+            git_repos.push(path.clone());
+            if nested {
+                git_repos.extend(find_git_repos(&path, nested));
+            }
+            continue;
+        }
 
-    let (repos, dirs): (Vec<_>, Vec<_>) = entries
-        .par_iter()
-        .filter(|entry| entry.path().is_dir())
-        .partition(|entry| entry.path().join(".git").exists());
-
-    let mut git_repos: Vec<_> = repos.par_iter().map(|entry| entry.path()).collect();
-
-    let nested_dirs: Vec<_> = dirs
-        .par_iter()
-        .flat_map(|entry| find_git_repos(&entry.path(), nested))
-        .collect();
-
-    git_repos.extend(nested_dirs);
-
-    if nested {
-        let nested_repos: Vec<_> = repos
-            .par_iter()
-            .flat_map(|entry| find_git_repos(&entry.path(), nested))
-            .collect();
-
-        git_repos.extend(nested_repos)
+        git_repos.extend(find_git_repos(&path, nested));
     }
 
     git_repos
@@ -172,4 +152,51 @@ fn fzf_select(choices: &[String]) -> Result<String, Box<dyn std::error::Error>> 
     let output = child.wait_with_output()?;
     let selected = String::from_utf8_lossy(&output.stdout).trim().to_string();
     Ok(selected)
+}
+
+fn is_tmux_running() -> bool {
+    env::var("TMUX").is_ok()
+        || Command::new("pgrep")
+            .arg("tmux")
+            .output()
+            .map_or(false, |o| o.status.success())
+}
+
+fn start_tmux_session(session_name: &str, path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    Command::new("tmux")
+        .arg("new-session")
+        .arg("-s")
+        .arg(session_name)
+        .arg("-c")
+        .arg(path)
+        .status()?;
+    Ok(())
+}
+
+fn switch_tmux_client(session_name: &str, path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let has_session = Command::new("tmux")
+        .arg("has-session")
+        .arg("-t")
+        .arg(session_name)
+        .output()?
+        .status
+        .success();
+
+    if !has_session {
+        Command::new("tmux")
+            .arg("new-session")
+            .arg("-ds")
+            .arg(session_name)
+            .arg("-c")
+            .arg(path)
+            .status()?;
+    }
+
+    Command::new("tmux")
+        .arg("switch-client")
+        .arg("-t")
+        .arg(session_name)
+        .status()?;
+
+    Ok(())
 }
